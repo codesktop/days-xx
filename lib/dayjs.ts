c4be70b7d32df748f64d1a7d7ece919869a3c31f
neweos <65535758+neweos@users.noreply.github.com>
2022-07-26 17:38:27 +0800

    reset repository

--- lib/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import isBetween from 'dayjs/plugin/isBetween'
-import isToday from 'dayjs/plugin/isToday'
-import dayjs from 'dayjs'
-
-dayjs.extend(isBetween)
-dayjs.extend(isToday)
-
-export default dayjs
